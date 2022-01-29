cfa27de66fbc934ca6e3dcd6c88145a5d400e4af
Amaljith <amaljith64@gmail.com>
2022-01-29 17:06:57 +0530

    TODO MODEL CREATION

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class task(models.Model):
+    def __str__(self):
+        return self.name  # TO change name to added task
+
+    name = models.CharField(max_length=100)
+    priority = models.IntegerField()
+
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import task
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('hello')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        priority = request.POST.get('priority')
+        obj = task(name=name, priority=priority)
+        obj.save()
+    return render(request, 'home.html')
